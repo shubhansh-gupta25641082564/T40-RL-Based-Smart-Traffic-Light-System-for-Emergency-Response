a01a57232a91cf4852209f91688e1316e6f13d0e
Manas Kashyap <kashyap.manas.glams@gmail.com>
2025-11-06 02:24:44 +0530

    DQN Agent Bug Fixes

--- src/agent/replay_buffer.py
+++ src/agent/replay_buffer.py
@@ -1,54 +1,20 @@
 import random
 import numpy as np
-from collections import deque
 
 class ReplayBuffer:
-    """
-    Experience replay buffer for DQN
-    """
-
-    def __init__(self, capacity=10000):
-        """
-        Initialize replay buffer
-
-        Args:
-            capacity: Maximum number of experiences to store
-        """
-        self.buffer = deque(maxlen=capacity)
+    def __init__(self, buffer_size):
+        self.buffer = []
+        self.buffer_size = buffer_size
 
     def push(self, state, action, reward, next_state, done):
-        """
-        Add experience to buffer
-
-        Args:
-            state: Current state
-            action: Action taken
-            reward: Reward received
-            next_state: Next state
-            done: Whether episode ended
-        """
-        experience = (state, action, reward, next_state, done)
-        self.buffer.append(experience)
+        if len(self.buffer) >= self.buffer_size:
+            self.buffer.pop(0)
+        self.buffer.append((state, action, reward, next_state, done))
 
     def sample(self, batch_size):
-        """
-        Sample batch of experiences
-
-        Args:
-            batch_size: Number of experiences to sample
-        Returns:
-            Tuple of batched experiences
-        """
         batch = random.sample(self.buffer, batch_size)
-        states, actions, rewards, next_states, dones = zip(*batch)
-        return (
-            np.array(states),
-            np.array(actions),
-            np.array(rewards, dtype=np.float32),
-            np.array(next_states),
-            np.array(dones, dtype=np.uint8)
-        )
+        states, actions, rewards, next_states, dones = map(np.array, zip(*batch))
+        return states, actions, rewards, next_states, dones
 
     def __len__(self):
-        """Return current size of buffer"""
         return len(self.buffer)
