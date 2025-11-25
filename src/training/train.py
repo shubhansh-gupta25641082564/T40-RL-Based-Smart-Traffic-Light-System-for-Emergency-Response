a33903b4eeb84155e1ebbc0bceb5f3d75ec9ea73
Manas Kashyap <kashyap.manas.glams@gmail.com>
2025-11-25 23:36:03 +0530

    Unnecessary Files Deleted and Training Script Modified

--- src/training/train.py
+++ src/training/train.py
@@ -1,6 +1,7 @@
 import os
 import yaml
 import numpy as np
+import matplotlib.pyplot as plt
 import torch
 from tqdm import tqdm
 from src.environment.traffic_environment import TrafficEnvironment
@@ -60,6 +61,32 @@ def train(config):
     np.save(os.path.join(config["paths"]["logsdir"], "losses.npy"), episodelosses)
     env.close()
 
+def plot_training_logs(logsdir):
+    reward_file = os.path.join(logsdir, "rewards.npy")
+    loss_file = os.path.join(logsdir, "losses.npy")
+
+    rewards = np.load(reward_file)
+    losses = np.load(loss_file)
+
+    plt.figure(figsize=(12,4))
+
+    plt.subplot(1,2,1)
+    plt.plot(rewards)
+    plt.title("Reward per Episode")
+    plt.xlabel("Episode")
+    plt.ylabel("Reward")
+
+    plt.subplot(1,2,2)
+    plt.plot(losses)
+    plt.title("Loss per Episode")
+    plt.xlabel("Episode")
+    plt.ylabel("Loss (Total per Episode)")
+
+    plt.tight_layout()
+    plt.savefig(os.path.join(logsdir, "training_plot.png"))
+    plt.show()
+
 if __name__ == "__main__":
     config = load_config("config.yaml")
     train(config)
+    plot_training_logs(config["paths"]["logsdir"])
